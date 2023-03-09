0c36caa1d861bda9dbb9e0a3df661a3fe0997016
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-03-08 20:41:01 -0500

    fix app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,5 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.0.4"
-	id("io.spring.dependency-management") version "1.1.0"
-}
-
-group = "com.nisum.service"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
 }
 
 tasks.withType<Test> {
